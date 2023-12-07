9a93f472287dcf881e3371c97f37c3d2f99f9787
Rune Harlyk <rune@harlyk.dk>
2023-12-07 22:09:03 +0100

    Makes a crude prototype overview

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,71 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { lerp } from "three/src/math/MathUtils.js";
+
+	export let data:prototype;
+    let card:HTMLDivElement
+    let height = 0
+    let width = 0
+    let interval: number
+    let position = {x:0, y:0}
+    let last_trans = ""
+
+    onMount(() => {
+        height = card.clientHeight
+        width = card.clientWidth
+    })
+
+    const handleMouseMove = (event:MouseEvent) => {
+        position.x = event.layerX || 0
+        position.y = event.layerY || 0
+        updateRotation()
+    }
+
+    const handleMouseLeave = () => {
+        clearInterval(interval)
+        console.log("Staring interval");
+        position = {x:card.clientWidth/2, y:card.clientHeight/2}
+        interval = setInterval(() =>  { 
+            if(updateRotation()) clearInterval(interval)
+        }, 1000/60)
+    }
+
+    const handleMouseEnter = () =>  {
+        clearInterval(interval)
+        interval = setInterval(() =>  { updateRotation() }, 1000/60)
+    }
+
+    const updateRotation = () => {
+        if(!card) return
+        
+        const transform = card.style.transform
+        const extractRotation = (axis:string) => parseFloat((transform.match(new RegExp(`${axis}\\(([^)]+)deg\\)`)) || [])[1]) || 0;
+        const xRot = extractRotation('rotateX')
+        const yRot = extractRotation('rotateY')
+        const yRotation = Math.floor(lerp(yRot, 10 * ((position.x - width / 2) / width), 0.1) * 100) / 100
+        const xRotation = Math.floor(lerp(xRot, -10 * ((position.y - height / 2) / height), 0.1) * 100) / 100
+        const string = 'perspective(1000px) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+        if (last_trans == string) return true
+        last_trans = string
+        card.style.transform = string
+    }
+
+</script>
+
+<!-- svelte-ignore a11y-no-static-element-interactions -->
+<!-- svelte-ignore a11y-mouse-events-have-key-events -->
+<div
+    on:mouseenter={handleMouseEnter}
+    on:mousemove={handleMouseMove} 
+    on:mouseout={handleMouseLeave} 
+    class="aspect-video hover:transition-none transition-transform z-20">
+    <div bind:this={card} class="h-full flex justify-center">  
+        <div class="w-full -z-10 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+            <img src={data.framed_demo} class="object-cover w-full h-full -z-10" alt="">
+            <div class="">
+                {data.description}
+                {data.name}
+            </div>
+        </div>
+    </div>
+</div>
---
+++ src/routes/prototypes/+page.svelte
@@ -0,0 +1,55 @@
+<script>
+    import Card from '$lib/components/Card.svelte';
+import prototypes from './prototypes.json'
+</script>
+<div>
+	<div class="-z-10 h-screen sky flex content-center items-center">
+		<div class="z-10 w-3/4 h-full flex items-center justify-center flex-col">
+			<div
+				class="w-2/3 p-6 bg-gray-200 rounded-md bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-20 border border-gray-100"
+			>
+				<h2 class="text-3xl">Prototypes</h2>
+                <div class="grid grid-flow-row-dense grid-cols-3 grid-rows-3 ...">
+                    <div class="col-span-2">01</div>
+                    <div class="col-span-2">02</div>
+                    <div>03</div>
+                    <div>04</div>
+                    <div>05</div>
+                  </div>
+			</div>
+		</div>
+		<div class="w-1/4 h-full" />
+	</div>
+
+	<div class="h-screen ocean">
+        <div class="grid grid-cols-1 md:grid-cols-2 grid-flow-row hover:grid-flow-row-dense gap-10 md:gap-24 auto-cols-max pt-20 px-10 md:px-20">
+            {#each prototypes as proto}
+                <Card data={proto}></Card>
+            {/each}
+        </div>
+		<footer class="fixed bottom-0 flex h-16 w-full text-zinc-50 justify-between p-4">
+			<div />
+			<div>Made with ❤️ and 🕰️ by Rune Harlyk</div>
+		</footer>
+	</div>
+</div>
+
+<style>
+	.sky {
+		background: #2980b9;
+		background: -webkit-linear-gradient(to top, #6dd5fa, #6dd5fa, #2980b9);
+		background: linear-gradient(to top, #6dd5fa, #6dd5fa, #2980b9);
+	}
+
+	.ocean {
+		min-height: 400vh;
+		background: #373b44;
+		background: -webkit-linear-gradient(to bottom, #4286f4, #373b44);
+		background: linear-gradient(to bottom, #4286f4, #252d40, #252d40, #070a0f);
+	}
+
+	* {
+		border: 1px solid red;
+		box-sizing: border-box;
+	}
+</style>
