ed4eeb1a4aa8b4d83710e8c1b709ce1dde7c4aa5
Rune Harlyk <rune@harlyk.dk>
2023-12-07 15:09:24 +0100

    Shows the framed demo of prototypes

--- src/routes/prototypes/[slug]/+page.svelte
+++ src/routes/prototypes/[slug]/+page.svelte
@@ -3,5 +3,18 @@
 	export let data;
 </script>
 
-<h1>{data.name}</h1>
-<div>{@html data.description}</div>
+<div>
+	<div class="flex divide-x">
+		{#each data.keywords || [] as keyword}
+			<div>{keyword}</div>
+		{/each}
+	</div>
+	<h1>{data.name}</h1>
+	<div>{@html data.description}</div>
+
+	{#if data['framed-demo']}
+		<iframe src={data['framed-demo']} title="demo"></iframe>
+	{/if}
+
+</div>
+
