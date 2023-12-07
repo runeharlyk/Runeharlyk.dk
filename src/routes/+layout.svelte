643c6740c21c32466132e0a7d7d798e2c109bb49
Rune Harlyk <rune@harlyk.dk>
2023-12-07 23:46:20 +0100

    Removes unused imports

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,5 @@
 <script>
-	import { Canvas } from '@threlte/core';
 	import '../app.css';
-	import Scene from '$lib/components/Scene.svelte';
 	import Background from '$lib/components/Background.svelte';
 </script>
 
