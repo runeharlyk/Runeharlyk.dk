77c227cd1a2535cf27bcfb6555cb159097b16d45
Rune Harlyk <rune@harlyk.dk>
2023-12-07 14:39:42 +0100

    ✨ Adds initial project setup

---
+++ src/lib/components/App.svelte
@@ -0,0 +1,3 @@
+
+<div class="fixed top-0 h-full w-full">
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import App from '$lib/components/App.svelte'
+</script>
+
+<div>
+  <App />
+</div>
+
+<style>
+  :global(body) {
+    margin: 0;
+    font-family: 'Roboto', sans-serif;
+    color: white;
+  }
+</style>
