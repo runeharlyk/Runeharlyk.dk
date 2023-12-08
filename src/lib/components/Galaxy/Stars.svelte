2e44d5822ea0c3bd5671a60d6c76e9fce198cd19
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:19:21 +0100

    Adds stars component

---
+++ src/lib/components/Galaxy/Stars.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { T } from '@threlte/core'
+    import { InstancedMesh, Instance } from '@threlte/extras'
+	import { randomPointOnSphere } from './utils';
+
+    export let stars:number = 10000
+    export let radius:number = 1000
+    export let color:number = 0xffcc6f
+
+    const positions = Array.from({ length: stars }, () => randomPointOnSphere(radius));
+</script>
+
+<InstancedMesh>  
+    <T.SphereGeometry />
+    <T.MeshBasicMaterial transparent color="#ffffff" ></T.MeshBasicMaterial>
+
+    {#each positions as position}
+        <Instance position={position} color={color}/>
+    {/each}
+</InstancedMesh>
+
