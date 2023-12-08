f965a0d486985749834cf9103d60be7864da15ac
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:20:16 +0100

    Adds a probability based startype

--- src/lib/components/Galaxy/config/starDistributions.js
+++ src/lib/components/Galaxy/config/starDistributions.js
@@ -1,5 +1,14 @@
 export const starTypes = {
-    percentage : [76.45, 12.1, 7.6, 3.0, 0.6, 0.13],
-    color: [0xffcc6f, 0xffd2a1, 0xfff4ea, 0xf8f7ff, 0xcad7ff, 0xaabfff],
-    size: [0.7, 0.7, 1.15, 1.48, 2.0, 2.5, 3.5]
-}
+	percentage: [80, 50, 20, 15, 10, 5],
+	color: [0xffcc6f, 0xffd2a1, 0xfff4ea, 0xf8f7ff, 0xcad7ff, 0xaabfff],
+	size: [0.7, 0.7, 1.15, 1.48, 2.0, 2.5, 3.5]
+};
+
+export const getStarType = () => {
+	const num = Math.random() * 100;
+
+	for (const [i, percentage] of starTypes.percentage.reverse().entries()) {
+		if (num < percentage) return i;
+	}
+	return 0;
+};
