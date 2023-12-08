6ecea655a96af2057406760376efaeabcd9ab773
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:23:21 +0100

    Makes layout dependent on route

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,10 +3,16 @@
 	import Background from '$lib/components/Background.svelte';
 	import Header from '$lib/components/Header.svelte';
     import Footer from '$lib/components/Footer.svelte';
+	import { page } from '$app/stores';
+	$: console.log($page.params);
 </script>
 
-<Background />
+{#if $page.route.id?.includes('/scenes/')}
+  <slot />
+{:else}
+  <Background scene="Galaxy"/>
+  <Header />
+  <slot />
+  <Footer />
+{/if}
 
-<Header />
-<slot />
-<Footer />
