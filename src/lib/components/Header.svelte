3b64c48cadd8309c15e6458561eb6719029a5aa0
Rune Harlyk <rune@harlyk.dk>
2023-12-08 15:28:13 +0100

    Adds routes.json

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,10 +1,41 @@
-<header class="w-full flex fixed h-16 items-center justify-between z-20">
-	<div class="pl-5 text-2xl">✨</div>
-	<div class="flex items-center justify-evenly w-96">
-		<a href="/work">Work</a>
-		<a href="/prototypes">Prototypes</a>
-		<a href="/info">Info</a>
-		<a href="/social">Socials</a>
-	</div>
-	<div class="pr-5 text-2xl">✨</div>
+<script>
+	import routes from '$lib/routes.json'
+	let open = false
+
+	const toggle = () => {
+		open = !open
+	}
+</script>
+<header class="fixed w-full flex h-16 md:items-center justify-end md:justify-center z-20 text-content">
+	<!-- <button on:click={toggle} data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center m-2 p-2 w-14 h-14 justify-center text-sm text-gray-500 rounded-lg md:hidden"aria-controls="navbar-default" aria-expanded="false">
+		<span class="sr-only">Open main menu</span>
+		<svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+			<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+		</svg>
+	</button> -->
+
+	
+	<nav class="flex items-center w-96 divide-x divide-gray-900 max-md:hidden">
+		<div >
+			{#each routes as route}
+			<a class="px-4" href={route.route}>{route.name}</a>
+			{/each}
+		</div>
+	</nav>
 </header>
+
+<!-- <div class:hidden={open} class="fixed left-0 z-30 h-screen w-screen flex justify-end">
+	<nav class={`bg-red-700 transition-all h-full ${open ? 'w-2/3': ''}`}>
+		<button on:click={toggle} data-collapse-toggle="navbar-default" type="button" class="inline-flex items-center m-2 p-2 w-14 h-14 justify-center text-sm text-gray-500 rounded-lg md:hidden"aria-controls="navbar-default" aria-expanded="false">
+			<span class="sr-only">Open main menu</span>
+			<svg class="w-8 h-8" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+				<path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
+			</svg>
+		</button>
+		<div class="flex flex-col">
+			{#each routes as route}
+				<a class="px-4" href={route.route}>{route.name}</a>
+			{/each}
+		</div>
+	</nav>
+</div> -->
