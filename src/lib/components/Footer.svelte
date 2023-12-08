a0cbd3b2e167bb90460dea99718ca04ba13b954d
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:21:53 +0100

    Makes footer left placed

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,4 +1,4 @@
-<footer class="fixed bottom-0 flex h-16 w-screen text-zinc-50 justify-between items-center">
+<footer class="fixed left-0 bottom-0 flex h-16 w-screen text-zinc-50 justify-between items-center">
     <div />
     <div class="mr-8">Made with ❤️ and 🕰️ by Rune Harlyk</div>
 </footer>
