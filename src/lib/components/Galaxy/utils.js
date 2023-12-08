fa02ec30b570ac8c7b97dfb4d246434269792fef
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:19:55 +0100

    Adds function to generate points on a circle and in galaxy formation

--- src/lib/components/Galaxy/utils.js
+++ src/lib/components/Galaxy/utils.js
@@ -1,4 +1,3 @@
-import { Vector3 } from 'three';
 import { ARM_X_DIST, SPIRAL } from './config/galaxyConfig.js';
 
 export function gaussianRandom(mean = 0, stdev = 1) {
@@ -18,5 +17,57 @@ export function spiral(x, y, z, offset) {
 	let theta = offset;
 	theta += x > 0 ? Math.atan(y / x) : Math.atan(y / x) + Math.PI;
 	theta += (r / ARM_X_DIST) * SPIRAL;
-	return new Vector3(r * Math.cos(theta), r * Math.sin(theta), z);
+	return [r * Math.cos(theta), r * Math.sin(theta), z];
 }
+
+export function randomPointOnSphere(radius) {
+	var theta = Math.random() * 2 * Math.PI;
+	var phi = Math.acos(2 * Math.random() - 1);
+	var x = radius * Math.sin(phi) * Math.cos(theta);
+	var y = radius * Math.sin(phi) * Math.sin(theta);
+	var z = radius * Math.cos(phi);
+	return [x, y, z];
+}
+
+export const generatePositions = (
+	stars,
+	arms_density,
+	arm_x_mean,
+	arm_x,
+	arm_y_mean,
+	arm_y,
+	thickness,
+	core_x,
+	core_y,
+	outer_core_x
+) => {
+	let positions = [];
+
+	for (let j = 0; j < arms_density; j++) {
+		for (let i = 0; i < stars / 4; i++) {
+			let pos = spiral(
+				gaussianRandom(arm_x_mean, arm_x),
+				gaussianRandom(arm_y_mean, arm_y),
+				gaussianRandom(0, thickness),
+				(j * 2 * Math.PI) / arms_density
+			);
+			positions.push(pos);
+		}
+	}
+
+	for (let i = 0; i < stars / 4; i++) {
+		let pos = [gaussianRandom(0, core_x), gaussianRandom(0, core_y), gaussianRandom(0, thickness)];
+		positions.push(pos);
+	}
+
+	for (let i = 0; i < stars / 4; i++) {
+		let pos = [
+			gaussianRandom(0, outer_core_x),
+			gaussianRandom(0, core_x),
+			gaussianRandom(0, thickness)
+		];
+		positions.push(pos);
+	}
+
+	return positions;
+};
