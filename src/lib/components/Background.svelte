26a75406d94fc5b76964c775a932fb7a693649bb
Rune Harlyk <rune@harlyk.dk>
2023-12-08 21:21:34 +0100

    Makes background component dynamic

--- src/lib/components/Background.svelte
+++ src/lib/components/Background.svelte
@@ -1,22 +1,27 @@
-<script>
+<script lang="ts">
 	import { Canvas } from '@threlte/core';
-	import Scene from '$lib/components/Galaxy/SpaceScene.svelte';
+	import SpaceScene from '$lib/components/Galaxy/SpaceScene.svelte';
+	import OceanScene from '$lib/components/OceanScene.svelte';
+
+    type BackgroundScene = "Galaxy" | "Ocean"
+
+    export let scene:BackgroundScene
 </script>
-<div class="fixed top-0 h-full w-full -z-10 bg-deep-space">
+
+<div class="fixed top-0 h-full w-full -z-10 bg-{scene}">
     <Canvas>
-        <Scene />
+        {#if scene === "Galaxy"}
+            <SpaceScene />
+        {:else if scene === "Ocean"}
+            <OceanScene />
+        {/if}
     </Canvas>
 </div>
 
 <style>
-    .bg-ocean {
-        background: #000046;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #1CB5E0, #000046);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to bottom, #1CB5E0, #000046); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
-    }
-    .bg-deep-space {
-        background: #4b6cb7;  /* fallback for old browsers */
-        background: -webkit-linear-gradient(to right, #182848, #4b6cb7);  /* Chrome 10-25, Safari 5.1-6 */
-        background: linear-gradient(to right, #182848, #4b6cb7); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+    .bg-Galaxy {
+        background: #000428;  /* fallback for old browsers */
+        background: -webkit-linear-gradient(to right, #004e92, #000428);  /* Chrome 10-25, Safari 5.1-6 */
+        background: linear-gradient(to right, #004e92, #000428); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
     }
 </style>
