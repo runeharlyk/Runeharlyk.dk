4127badf414a1e148d27ac35f94aff96744444ed
Rune Harlyk <rune@harlyk.dk>
2023-12-25 17:27:59 +0100

    Change path to absolute for prototypes

--- src/routes/prototypes/[slug]/+page.ts
+++ src/routes/prototypes/[slug]/+page.ts
@@ -3,8 +3,10 @@ import prototypes from '../prototypes.json'
 
 /** @type {import('./$types').PageLoad} */
 export function load({ params }) {
-	const pageContent = prototypes.find((x) => x.path == params.slug);
-	const nextPageId = (prototypes.findIndex((x) => x.path == params.slug) + 1) % prototypes.length;
+	const relativePage = '/prototypes/';
+	const pageContent = prototypes.find((x) => x.path == relativePage + params.slug);
+	const nextPageId =
+		(prototypes.findIndex((x) => x.path == relativePage + params.slug) + 1) % prototypes.length;
 	const nextPage = prototypes[nextPageId].path;
 
 	if (!pageContent) throw error(404, 'Not found');
