d4794b19e855812005ae9503a13c729dacae87f6
Rune Harlyk <rune@harlyk.dk>
2023-12-25 14:22:42 +0100

    Adds scroll icon on prototypes page

--- src/routes/prototypes/+page.svelte
+++ src/routes/prototypes/+page.svelte
@@ -3,12 +3,53 @@
     import prototypes from './prototypes.json'
 </script>
 <div>
-	<div class="-z-10 h-screen sky flex flex-col justify-items-center pt-14 p-10 bg-opacity-60 text-content">
+	<div class="-z-10 h-screen sky flex flex-col justify-items-center pt-14 p-10 bg-opacity-60">
         <div class="">
             <h1 class="text-3xl">Prototype [noun]</h1>
             <h2>/ˈprəʊtətʌɪp/</h2>
             <em>First or preliminary version of a device or vehicle from which other forms are developed.</em>
         </div>
+        
+        <div>
+            <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            class="pulse-1 -mb-[20px]"
+            fill="#fff"
+            viewBox="0 0 256 256"
+            >
+            <path
+            d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+            />
+        </svg>
+        
+        <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        class="pulse-2 -mb-[20px]"
+        fill="#fff"
+        viewBox="0 0 256 256"
+        >
+        <path
+        d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+        />
+    </svg>
+    
+    <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="32"
+          height="32"
+          class="pulse-3 -mb-[20px]"
+          fill="#fff"
+          viewBox="0 0 256 256"
+          >
+          <path
+          d="M212.24,100.24l-80,80a6,6,0,0,1-8.48,0l-80-80a6,6,0,0,1,8.48-8.48L128,167.51l75.76-75.75a6,6,0,0,1,8.48,8.48Z"
+          />
+        </svg>
+    </div>
 	</div>
 
 	<div class="h-screen ocean">
